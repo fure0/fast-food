566ab65b979fa59f5a79f8167258a1a3cf14365a
KimTaeyoun <kimty1121@gmail.com>
2020-06-25 03:38:36 +0900

    design start order tap

--- src/views/OrderList.vue
+++ src/views/OrderList.vue
@@ -1,8 +1,38 @@
 <template>
-    <v-container fill-height>
-        <div>
-            List
-        </div>
+    <v-container>
+        <v-row>
+            <v-col>
+                <v-row>
+                    <v-col>
+                        <h3>Cocking</h3>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col>
+                        <v-card max-width="200">
+                            <v-card-title class="pb-0">Order No</v-card-title>
+                            <v-card-text class="text--primary">
+                                List
+                            </v-card-text>
+                            <v-card-text class="text--primary">
+                                Waiting Time
+                            </v-card-text>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-col>
+            <v-col>
+                <v-row>
+                    <v-col>
+                        <h3>Done</h3>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col>
+                    </v-col>
+                </v-row>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
